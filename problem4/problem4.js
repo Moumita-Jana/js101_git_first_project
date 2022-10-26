7c544319a60e11d9ddc1bb31be8b0918803ca09a
Moumita-Jana <moumitajana1004@gmail.com>
2022-10-26 23:10:22 +0530

    Donald

---
+++ problem4/problem4.js
@@ -0,0 +1,16 @@
+let database_user="xyz@gmail.com";
+let database_pass="abc4545";
+let user="xyz@gmail.com";
+let pass="abc4545";
+if(database_user==user){
+  if(database_pass==pass){
+    console.log("Login Successfully");
+    
+  }else{
+    console.log("Incorrect Password");
+    
+  } 
+}else{
+    console.log("Wrong Input")
+  
+}
